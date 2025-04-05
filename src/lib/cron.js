c2b7f5830cbf62003c8885094333c4daa5485c3d
NureniJamiu <nurenijamiu951@gmail.com>
2025-04-05 08:23:19 -0700

    feat: add cron job to make request after every 14 minutes

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -2,12 +2,14 @@ import cron from "cron";
 import https from "https";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
-    https.get(process.env.API_URL, (res) => {
-        if (res.statusCode +== 200) console.log("Get request sent successfully.");
-        else console.log("Get request failed.", res.statusCode);
-    }).on("error", (e) => {
-        console.log("Error while sending request", e);
+  https
+    .get(process.env.API_URL, (res) => {
+      if (res.statusCode === 200) console.log("Get request sent successfully.");
+      else console.log("Get request failed.", res.statusCode);
+    })
+    .on("error", (e) => {
+      console.log("Error while sending request", e);
     });
-})
+});
 
-export default job
+export default job;
