75025f8af9525eff4762c0e10d901692ade3bce6
NureniJamiu <nurenijamiu951@gmail.com>
2025-04-05 07:44:59 -0700

    feat: add cron job to make request after every 14 minutes

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -2,7 +2,7 @@ import cron from "cron";
 import https from "https";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
-    https.get("https://booksarena-backend.onrender.com/api/cron", (res) => {
+    https.get(process.env.API_URL, (res) => {
         if (res.statusCode +== 200) console.log("Get request sent successfully.");
         else console.log("Get request failed.", res.statusCode);
     }).on("error", (e) => {
